394cae3fa18847a39e9b3ff558a5c737c79de420
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-07-25 02:29:20 -0400

    redux

---
+++ v0.1/deno/cli/run-jackrabbit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+current_dir=`dirname $0`
+
+target_pathname=$current_dir/mod.ts
+
+deno run --allow-read $target_pathname --file $1
--- v0.1/es/core/jackrabbit.js
+++ v0.1/es/core/jackrabbit.js
@@ -1,3 +1,7 @@
+// deno-fmt-ignore-file
+// deno-lint-ignore-file
+// This code was bundled using `deno bundle` and it's not recommended to edit it manually
+
 const PENDING = "pending";
 const UNSUBMITTED = "unsubmitted";
 const CANCELLED = "cancelled";
@@ -10,315 +14,310 @@ const START_COLLECTION = "start_collection";
 const END_COLLECTION = "end_collection";
 const START_TEST = "start_test";
 const END_TEST = "end_test";
-const sleep = (time) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, time);
-  });
+const sleep = (time)=>{
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        }, time);
+    });
 };
-const createTimeout = async (timeoutInterval) => {
-  const interval = timeoutInterval === -1 ? 10000 : timeoutInterval;
-  await sleep(interval);
-  return [
-    `timed out at: ${timeoutInterval}`,
-  ];
+const createTimeout = async (timeoutInterval)=>{
+    const interval = timeoutInterval === -1 ? 10000 : timeoutInterval;
+    await sleep(interval);
+    return [
+        `timed out at: ${timeoutInterval}`
+    ];
 };
 async function execRun(store) {
-  const startTime = performance.now();
-  store.dispatch({
-    type: START_RUN,
-    startTime,
-  });
-  for (const collectionResult of store.data.collectionResults) {
-    if (runIsCancelled(store)) {
-      return;
+    const startTime = performance.now();
+    store.dispatch({
+        type: START_RUN,
+        startTime
+    });
+    for (const collectionResult of store.data.collectionResults){
+        if (runIsCancelled(store)) {
+            return;
+        }
+        collectionResult.runTestsAsynchronously ? await execCollection(store, collectionResult) : await execCollectionOrdered(store, collectionResult);
     }
-    collectionResult.runTestsAsynchronously
-      ? await execCollection(store, collectionResult)
-      : await execCollectionOrdered(store, collectionResult);
-  }
-  const endTime = performance.now();
-  if (runIsCancelled(store)) {
-    return;
-  }
-  store.dispatch({
-    type: END_RUN,
-    endTime,
-  });
+    const endTime = performance.now();
+    if (runIsCancelled(store)) {
+        return;
+    }
+    store.dispatch({
+        type: END_RUN,
+        endTime
+    });
 }
 function cancelRun(store) {
-  store.dispatch({
-    type: CANCEL_RUN,
-    endTime: performance.now(),
-  });
+    store.dispatch({
+        type: CANCEL_RUN,
+        endTime: performance.now()
+    });
 }
 async function execTest(store, testResult, timeoutInterval) {
-  const { testResultID } = testResult;
-  const testFunc = store.data.tests[testResultID];
-  const startTime = performance.now();
-  store.dispatch({
-    type: START_TEST,
-    testResultID,
-    startTime,
-  });
-  const assertions = testFunc !== undefined
-    ? await Promise.race([
-      createTimeout(timeoutInterval),
-      testFunc(),
-    ])
-    : [];
-  const endTime = performance.now();
-  if (runIsCancelled(store)) return;
-  store.dispatch({
-    type: END_TEST,
-    testResultID,
-    assertions,
-    endTime,
-  });
+    const { testResultID  } = testResult;
+    const testFunc = store.data.tests[testResultID];
+    const startTime = performance.now();
+    store.dispatch({
+        type: START_TEST,
+        testResultID,
+        startTime
+    });
+    const assertions = testFunc !== undefined ? await Promise.race([
+        createTimeout(timeoutInterval),
+        testFunc(), 
+    ]) : [];
+    const endTime = performance.now();
+    if (runIsCancelled(store)) return;
+    store.dispatch({
+        type: END_TEST,
+        testResultID,
+        assertions,
+        endTime
+    });
 }
 async function execCollection(store, collectionResuilt) {
-  if (runIsCancelled(store)) return;
-  const { indices, collectionResultID, timeoutInterval } = collectionResuilt;
-  const tests = [];
-  let target = indices[0];
-  const dest = indices[1];
-  while (target <= dest) {
-    const testResult = store.data.testResults[target];
-    if (testResult !== undefined) {
-      tests.push(execTest(store, testResult, timeoutInterval));
+    if (runIsCancelled(store)) return;
+    const { indices , collectionResultID , timeoutInterval  } = collectionResuilt;
+    const tests = [];
+    let target = indices[0];
+    const dest = indices[1];
+    while(target <= dest){
+        const testResult = store.data.testResults[target];
+        if (testResult !== undefined) {
+            tests.push(execTest(store, testResult, timeoutInterval));
+        }
+        target += 1;
     }
-    target += 1;
-  }
-  const startTime = performance.now();
-  store.dispatch({
-    type: START_COLLECTION,
-    collectionResultID,
-    startTime,
-  });
-  await Promise.all(tests);
-  const endTime = performance.now();
-  if (runIsCancelled(store)) return;
-  store.dispatch({
-    type: END_COLLECTION,
-    collectionResultID,
-    endTime,
-  });
+    const startTime = performance.now();
+    store.dispatch({
+        type: START_COLLECTION,
+        collectionResultID,
+        startTime
+    });
+    await Promise.all(tests);
+    const endTime = performance.now();
+    if (runIsCancelled(store)) return;
+    store.dispatch({
+        type: END_COLLECTION,
+        collectionResultID,
+        endTime
+    });
 }
 async function execCollectionOrdered(store, collectionResuilt) {
-  if (runIsCancelled(store)) return;
-  const { indices, collectionResultID, timeoutInterval } = collectionResuilt;
-  const dest = indices[1];
-  let target = indices[0];
-  const startTime = performance.now();
-  store.dispatch({
-    type: START_COLLECTION,
-    collectionResultID,
-    startTime,
-  });
-  while (target < dest) {
     if (runIsCancelled(store)) return;
-    const testResult = store.data.testResults[target];
-    if (testResult !== undefined) {
-      await execTest(store, testResult, timeoutInterval);
+    const { indices , collectionResultID , timeoutInterval  } = collectionResuilt;
+    const dest = indices[1];
+    let target = indices[0];
+    const startTime = performance.now();
+    store.dispatch({
+        type: START_COLLECTION,
+        collectionResultID,
+        startTime
+    });
+    while(target < dest){
+        if (runIsCancelled(store)) return;
+        const testResult = store.data.testResults[target];
+        if (testResult !== undefined) {
+            await execTest(store, testResult, timeoutInterval);
+        }
+        target += 1;
     }
-    target += 1;
-  }
-  const endTime = performance.now();
-  if (runIsCancelled(store)) return;
-  store.dispatch({
-    type: END_COLLECTION,
-    collectionResultID,
-    endTime,
-  });
+    const endTime = performance.now();
+    if (runIsCancelled(store)) return;
+    store.dispatch({
+        type: END_COLLECTION,
+        collectionResultID,
+        endTime
+    });
 }
 function runIsCancelled(store) {
-  return store.data.result.status === CANCELLED;
+    return store.data.result.status === CANCELLED;
 }
 class Runner {
-  start(store) {
-    execRun(store);
-  }
-  cancel(store) {
-    cancelRun(store);
-  }
-  async run(store) {
-    await execRun(store);
-  }
+    start(store) {
+        execRun(store);
+    }
+    cancel(store) {
+        cancelRun(store);
+    }
+    async run(store) {
+        await execRun(store);
+    }
 }
 function updateResultProperties(storeData) {
-  const { result } = storeData;
-  let { status } = storeData.result;
-  let testTime = 0;
-  for (const collectionResult of storeData.collectionResults) {
-    if (status === UNSUBMITTED && collectionResult.status === FAILED) {
-      status = FAILED;
+    const { result  } = storeData;
+    let { status  } = storeData.result;
+    let testTime = 0;
+    for (const collectionResult of storeData.collectionResults){
+        if (status === UNSUBMITTED && collectionResult.status === FAILED) {
+            status = FAILED;
+        }
+        testTime += collectionResult.testTime;
     }
-    testTime += collectionResult.testTime;
-  }
-  result.status = status === UNSUBMITTED ? PASSED : status;
-  result.testTime = testTime;
+    result.status = status === UNSUBMITTED ? PASSED : status;
+    result.testTime = testTime;
 }
 function updateCollectionResult(storeData, collectionResult) {
-  let testTime = 0;
-  let { indices, status } = collectionResult;
-  const target = indices[1];
-  let index = indices[0];
-  while (index < target) {
-    const { result } = storeData;
-    if (status === UNSUBMITTED && result.status === FAILED) {
-      status = FAILED;
+    let testTime = 0;
+    let { indices , status  } = collectionResult;
+    const target = indices[1];
+    let index = indices[0];
+    while(index < target){
+        const { result  } = storeData;
+        if (status === UNSUBMITTED && result.status === FAILED) {
+            status = FAILED;
+        }
+        const { startTime , endTime  } = result;
+        testTime += endTime - startTime;
+        index += 1;
     }
-    const { startTime, endTime } = result;
-    testTime += endTime - startTime;
-    index += 1;
-  }
-  collectionResult.status = status === UNSUBMITTED ? PASSED : status;
-  collectionResult.testTime = testTime;
+    collectionResult.status = status === UNSUBMITTED ? PASSED : status;
+    collectionResult.testTime = testTime;
 }
 function start_run(storeData, action) {
-  if (action.type !== START_RUN) return;
-  const { startTime } = action;
-  const { result } = storeData;
-  result.status = UNSUBMITTED;
-  result.startTime = startTime;
+    if (action.type !== START_RUN) return;
+    const { startTime  } = action;
+    const { result  } = storeData;
+    result.status = UNSUBMITTED;
+    result.startTime = startTime;
 }
 function end_run(storeData, action) {
-  if (action.type !== END_RUN) return;
-  const { result } = storeData;
-  if (result.status === CANCELLED) return;
-  const { endTime } = action;
-  result.endTime = endTime;
-  updateResultProperties(storeData);
+    if (action.type !== END_RUN) return;
+    const { result  } = storeData;
+    if (result.status === CANCELLED) return;
+    const { endTime  } = action;
+    result.endTime = endTime;
+    updateResultProperties(storeData);
 }
 function cancel_run(storeData, action) {
-  if (action.type !== CANCEL_RUN) return;
-  const { result } = storeData;
-  result.status = CANCELLED;
-  result.endTime = action.endTime;
+    if (action.type !== CANCEL_RUN) return;
+    const { result  } = storeData;
+    result.status = CANCELLED;
+    result.endTime = action.endTime;
 }
 function start_collection(storeData, action) {
-  if (action.type !== START_COLLECTION) return;
-  const { collectionResultID, startTime } = action;
-  const collectionResult = storeData.collectionResults[collectionResultID];
-  if (collectionResult === undefined) {
-    return;
-  }
-  collectionResult.status = UNSUBMITTED;
-  collectionResult.startTime = startTime;
+    if (action.type !== START_COLLECTION) return;
+    const { collectionResultID , startTime  } = action;
+    const collectionResult = storeData.collectionResults[collectionResultID];
+    if (collectionResult === undefined) {
+        return;
+    }
+    collectionResult.status = UNSUBMITTED;
+    collectionResult.startTime = startTime;
 }
 function end_collection(storeData, action) {
-  if (action.type !== END_COLLECTION) return;
-  const { collectionResultID } = action;
-  const collectionResult = storeData.collectionResults[collectionResultID];
-  if (collectionResult === undefined) {
-    return;
-  }
-  const { endTime } = action;
-  collectionResult.endTime = endTime;
-  updateCollectionResult(storeData, collectionResult);
+    if (action.type !== END_COLLECTION) return;
+    const { collectionResultID  } = action;
+    const collectionResult = storeData.collectionResults[collectionResultID];
+    if (collectionResult === undefined) {
+        return;
+    }
+    const { endTime  } = action;
+    collectionResult.endTime = endTime;
+    updateCollectionResult(storeData, collectionResult);
 }
 function start_test(storeData, action) {
-  if (action.type !== START_TEST) return;
-  const { testResultID, startTime } = action;
-  const testResult = storeData.testResults[testResultID];
-  if (testResult === undefined) {
-    return;
-  }
-  testResult.status = UNSUBMITTED;
-  testResult.startTime = startTime;
+    if (action.type !== START_TEST) return;
+    const { testResultID , startTime  } = action;
+    const testResult = storeData.testResults[testResultID];
+    if (testResult === undefined) {
+        return;
+    }
+    testResult.status = UNSUBMITTED;
+    testResult.startTime = startTime;
 }
 function end_test(storeData, action) {
-  if (action.type !== END_TEST) return;
-  const { testResultID } = action;
-  const testResult = storeData.testResults[testResultID];
-  if (testResult === undefined) {
-    return;
-  }
-  const { assertions, endTime } = action;
-  testResult.assertions = assertions;
-  testResult.endTime = endTime;
-  testResult.status = assertions.length === 0 ? PASSED : FAILED;
+    if (action.type !== END_TEST) return;
+    const { testResultID  } = action;
+    const testResult = storeData.testResults[testResultID];
+    if (testResult === undefined) {
+        return;
+    }
+    const { assertions , endTime  } = action;
+    testResult.assertions = assertions;
+    testResult.endTime = endTime;
+    testResult.status = assertions.length === 0 ? PASSED : FAILED;
 }
 const reactions = {
-  start_run,
-  end_run,
-  cancel_run,
-  start_collection,
-  end_collection,
-  start_test,
-  end_test,
+    start_run,
+    end_run,
+    cancel_run,
+    start_collection,
+    end_collection,
+    start_test,
+    end_test
 };
 function createInitialData() {
-  return {
-    testResults: [],
-    collectionResults: [],
-    result: {
-      status: UNSUBMITTED,
-      endTime: 0,
-      startTime: 0,
-      testTime: 0,
-    },
-    tests: [],
-  };
+    return {
+        testResults: [],
+        collectionResults: [],
+        result: {
+            status: UNSUBMITTED,
+            endTime: 0,
+            startTime: 0,
+            testTime: 0
+        },
+        tests: []
+    };
 }
-const createTestResults = (storeData, tests) => {
-  const startIndex = storeData.testResults.length;
-  for (const test of tests) {
-    const testID = storeData.tests.length;
-    storeData.tests.push(test);
-    const testResultID = storeData.testResults.length;
-    storeData.testResults.push({
-      assertions: [],
-      endTime: 0,
-      name: test.name,
-      startTime: 0,
-      status: PENDING,
-      testResultID,
-      testID,
-    });
-  }
-  const endIndex = storeData.testResults.length;
-  return [
-    startIndex,
-    endIndex,
-  ];
+const createTestResults = (storeData, tests)=>{
+    const startIndex = storeData.testResults.length;
+    for (const test of tests){
+        const testID = storeData.tests.length;
+        storeData.tests.push(test);
+        const testResultID = storeData.testResults.length;
+        storeData.testResults.push({
+            assertions: [],
+            endTime: 0,
+            name: test.name,
+            startTime: 0,
+            status: PENDING,
+            testResultID,
+            testID
+        });
+    }
+    const endIndex = storeData.testResults.length;
+    return [
+        startIndex,
+        endIndex
+    ];
 };
-const createCollectionResults = (storeData, collections) => {
-  for (const collection of collections) {
-    const collectionResultID = storeData.collectionResults.length;
-    const { tests, title, runTestsAsynchronously, timeoutInterval } =
-      collection;
-    const indices = createTestResults(storeData, tests);
-    storeData.collectionResults.push({
-      endTime: 0,
-      testTime: 0,
-      startTime: 0,
-      status: PENDING,
-      collectionResultID,
-      indices,
-      timeoutInterval,
-      runTestsAsynchronously,
-      title,
-    });
-  }
+const createCollectionResults = (storeData, collections)=>{
+    for (const collection of collections){
+        const collectionResultID = storeData.collectionResults.length;
+        const { tests , title , runTestsAsynchronously , timeoutInterval  } = collection;
+        const indices = createTestResults(storeData, tests);
+        storeData.collectionResults.push({
+            endTime: 0,
+            testTime: 0,
+            startTime: 0,
+            status: PENDING,
+            collectionResultID,
+            indices,
+            timeoutInterval,
+            runTestsAsynchronously,
+            title
+        });
+    }
 };
 class Store {
-  data = createInitialData();
-  callback;
-  setup(run, callback) {
-    createCollectionResults(this.data, run);
-    this.callback = callback;
-  }
-  teardown() {
-    this.callback = undefined;
-  }
-  dispatch(action) {
-    const reaction = reactions[action.type];
-    if (reaction === undefined) return;
-    reaction(this.data, action);
-    this.callback?.(this.data, action);
-  }
+    data = createInitialData();
+    callback;
+    setup(run, callback) {
+        createCollectionResults(this.data, run);
+        this.callback = callback;
+    }
+    teardown() {
+        this.callback = undefined;
+    }
+    dispatch(action) {
+        const reaction = reactions[action.type];
+        if (reaction === undefined) return;
+        reaction(this.data, action);
+        this.callback?.(this.data, action);
+    }
 }
 export { Runner as Jackrabbit };
 export { Store as Store };
