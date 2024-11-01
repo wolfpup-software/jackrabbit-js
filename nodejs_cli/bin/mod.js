4d924932a82fcbd2267db2ea890bad37a4fb3c24
Taylor Vann <brian.t.vann@gmail.com>
2024-11-01 12:58:03 -0700

    update readmes

--- nodejs_cli/bin/mod.js
+++ nodejs_cli/bin/mod.js
@@ -3,14 +3,15 @@ import { Config, Importer, JackrabbitError, run } from "./deps.js";
 const config = new Config(process.argv.slice(2));
 const importer = new Importer(process.cwd());
 try {
-  await run(config, importer);
-} catch (e) {
-  if (e instanceof Error && !(e instanceof JackrabbitError)) {
-    console.log(`
+    await run(config, importer);
+}
+catch (e) {
+    if (e instanceof Error && !(e instanceof JackrabbitError)) {
+        console.log(`
 Error:
 ${e.name}
 ${e.message}
 ${e.stack}`);
-  }
-  process.exit(1);
+    }
+    process.exit(1);
 }
